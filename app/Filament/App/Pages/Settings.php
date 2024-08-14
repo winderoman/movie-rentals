2b35fdf194fb6526272437f0c7ea1b065db6997c
= <mrlibertadfinanciera09@gmail.com>
2024-08-14 10:05:19 -0500

    stogare remove gitignore

--- app/Filament/App/Pages/Settings.php
+++ app/Filament/App/Pages/Settings.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Filament\App\Pages;
-
+use Filament\Infolists\Components\ImageEntry;
 use Filament\Pages\Page;
 use App\Models\Movie;
 
 class Settings extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-film';
 
     protected static string $view = 'filament.app.pages.settings';
     protected static ?string $title = 'Rent Movie';
--- resources/views/filament/app/pages/settings.blade.php
+++ resources/views/filament/app/pages/settings.blade.php
@@ -1,11 +1,14 @@
 <x-filament-panels::page>
     <h1 class="text-gray-900 dark:text-gray-100">Películas Disponibles</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
         @foreach($this->getMovies() as $movie)
             <div class="bg-white dark:bg-gray-800 p-4 rounded-lg shadow">
-                {{-- <img src="{{ asset('storage/' . $movie->movie_image) }}" alt="{{ $movie->title }}" class="w-full h-48 object-cover rounded-md"> --}}
-                <h2 class="text-xl font-semibold mt-2 text-gray-900 dark:text-gray-100">{{ $movie->title }}</h2>
-                <p class="text-gray-700 dark:text-gray-300">{{ $movie->description }}</p>
+            <!-- <x-filament::avatar
+                src="{{ asset('storage/' . $movie->movie_image) }}"
+                alt="{{ $movie->title }}"
+                :circular="false"
+            /> -->
+                <img class="h-48 w-full rounded-lg object-cover" src="{{ asset('storage/' . $movie->movie_image) }}" alt="{{ $movie->title }}">
             </div>
         @endforeach
     </div>
